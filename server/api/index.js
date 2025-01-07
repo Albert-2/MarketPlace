2bfcca34f31df06b61db865433e6aee2c9545ea4
Bhushan <alberteinstein02.99792458>
2025-01-08 00:18:08 +0530

    feat(cors): Updated CORS policy

--- server/api/index.js
+++ server/api/index.js
@@ -9,7 +9,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
